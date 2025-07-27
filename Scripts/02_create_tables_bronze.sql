df737582b09321a439288f9f75ccbe614c027701
sai-krishna-akkala <newonebro113@gmail.com>
2025-07-27 13:17:56 +0530

    Update 02_create_tables_bronze.sql

--- Scripts/02_create_tables_bronze.sql
+++ Scripts/02_create_tables_bronze.sql
@@ -15,7 +15,7 @@ END;
 GO
 CREATE TABLE bronze.crm_cust_info (
     cst_id             INT,
-    cst_key            INT,
+    cst_key            NVARCHAR(50),
     cst_firstname      NVARCHAR(50),
     cst_lastname       NVARCHAR(50),
     cst_marital_status NVARCHAR(50),
