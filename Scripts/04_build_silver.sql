d736b7759933b0caf8d041686ddf599d60f4439c
sai-krishna-akkala <newonebro113@gmail.com>
2025-07-28 13:56:49 +0530

    Update 04_build_silver.sql

--- Scripts/04_build_silver.sql
+++ Scripts/04_build_silver.sql
@@ -181,7 +181,7 @@ BEGIN
         SET @start_t = GETDATE();
         INSERT INTO silver.erp_loc_a101(cid, cntry)
         SELECT 
-            REPLACE(cid, '_', ''),
+            REPLACE(cid, '-', ''),
             CASE
                 WHEN TRIM(cntry) = 'DE' THEN 'Germany'
                 WHEN TRIM(cntry) IN ('US', 'USA') THEN 'United States'
